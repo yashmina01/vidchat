12dba85f70b29bc7b0808cac5db1793e0d1414ac
Yash Mina <yashkhodia01@gmail.com>
2024-06-15 20:56:44 +0530

    Added buttom functionality

--- base/views.py
+++ base/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from agora_token_builder import RtcTokenBuilder
 
 # Create your views here.
+
+def getToken(request):
+    token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
+    return 
 
 def lobby(request):
     return render(request, 'base/lobby.html')
