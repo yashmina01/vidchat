217681e2fc674e283056d90fbaad4a6eec924520
Yash Mina <yashkhodia01@gmail.com>
2024-06-19 21:41:48 +0530

    added allowed host

--- vidchat/settings.py
+++ vidchat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!70yqlw&w7=5+pm*v49rsb1+*_9^q8tvi007&iew$=@8utcqkj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vidchat-production.up.railway.app']
 
 
 # Application definition
